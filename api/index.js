0211a002c126d3aff2cf09ed909ff1eb04faba9c
Ako Lath Auguste <te.lauguste@gmail.com>
2024-12-09 15:55:30 +0000

    connect to database

--- api/index.js
+++ api/index.js
@@ -1,7 +1,22 @@
 // import express from "express";
 const express = require("express");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+
+dotenv.config();
+
+const MONGO_URL = process.env.MONGO;
 
 const app = express();
+
+mongoose
+  .connect(MONGO_URL)
+  .then(() => {
+    console.log("Connected to MongoDB");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
 app.listen(3000, () => {
   console.log("Server is running on port 3000 !!");
